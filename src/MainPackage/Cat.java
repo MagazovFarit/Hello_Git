3ce466d58310a74d9c0adf897ffe1eb544cca06c
MagazovFarit <34268356+MagazovFarit@users.noreply.github.com>
2019-02-28 14:43:11 +0500

    Add files via upload

---
+++ src/MainPackage/Cat.java
@@ -0,0 +1,13 @@
+package MainPackage;
+
+public class Cat {
+
+    String name;
+    int year;
+
+    public  Cat(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+}
--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,7 +1,20 @@
 package MainPackage;
 
+import java.io.*;
+
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello GitHub!!!");
+        Cat cat1 = new Cat("Kotya", 14);
+        System.out.println(cat1.name);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        cat1.name = reader.readLine();
+        System.out.println(cat1.name);
+
+        String text;
+        while (!(text = reader.readLine()).isEmpty()){
+            System.out.println(text);
+        }
+
     }
 }
